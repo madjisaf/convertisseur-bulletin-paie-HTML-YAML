2afef5b93b4d685e6267bc7a760667273bd7f0f5
Safri Madjid <madjisaf@gmail>
2015-05-18 20:22:21 +0200

    Create YAML files upon execution
    Fix #4

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 #!/bin/env node
 
 
-var fs = require('fs');
+var fs = require('fs'),
+	path = require('path');
 
 var yaml = require('js-yaml');
 
 var parser = require('./src/parser'),
 	mapper = require('./src/openfiscaMapper');
 
-var SOURCE = __dirname + '/assets/source.html';
+var SOURCE = path.join(__dirname, '/assets/source.html'),
+	TARGET_DIR = path.join(__dirname, '/dist');
+
+
+try {
+	fs.mkdirSync(TARGET_DIR);
+} catch (err) {
+	// do nothing: folder most probably already exists
+	// if it doesn't, all subsequent writes will complain anyway
+}
 
 parser.parse(fs.readFileSync(process.argv[2] || SOURCE))
 	  .forEach(function(payroll) {
-		console.log(yaml.safeDump(mapper.toOpenFisca(payroll)));
+	  	var targetPath = path.join(TARGET_DIR, payroll.id + '.yaml');
+
+	  	fs.writeFile(targetPath, yaml.safeDump(mapper.toOpenFisca(payroll)), function(err) {
+	  		if (err)
+	  			throw err;
+
+	  		console.log('Wrote file', targetPath);
+	  	});
 	  });
